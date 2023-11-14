64b8f9ebbb7b25700497d8f33aa2d7f9a9bcc1ba
Hudson Regis <hudsonregis@Hudsons-MacBook-Air.local>
2023-11-13 19:31:49 -0800

    Updated Callback Function

--- src/hpp/cpp/TestEngine.cpp
+++ src/hpp/cpp/TestEngine.cpp
@@ -4,23 +4,65 @@
 //Include Headers
 
 //Callback Function
-void callback
+static void callback
 (
 	const VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
 	const VkDebugUtilsMessageTypeFlagsEXT messageType,
 	const VkDebugUtilsMessengerCallbackDataEXT* messageData
 )
 {
-	if(messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)
+	if(messageSeverity < VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT || messageType < VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT)
 	{
-		if(messageType >= VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT)
+		return;
+	}
+
+	//Debug Message Information
+	std::cerr																			\
+	<< "[DEBUG " << messageData -> pMessageIdName										\
+	<< ": " << messageData -> messageIdNumber											\
+	<< "] " << messageData -> pMessage << "\n";
+
+	//Check if Queue Labels Exist
+	if(messageData -> queueLabelCount > 0 && messageData -> pQueueLabels)
+	{
+		//Debug Queue Labels
+		std::cerr << "Message Labels (" << messageData -> queueLabelCount << "):\n";
+
+		for(std::size_t i{}; i < messageData -> queueLabelCount; ++i)
 		{
-			std::cerr											\
-			<< "[DEBUG " << messageData -> pMessageIdName		\
-			<< ": " << messageData -> messageIdNumber			\
-			<< "] " << messageData -> pMessage << "\n\n";
+			std::cerr << "\t[" << i << "][QUEUE MESSAGE] \"" << messageData -> pQueueLabels[i].pLabelName << "\"\n";
 		}
 	}
+
+//	//Check if Objects Exist
+//	if(messageData -> objectCount > 0 && messageData -> pObjects)
+//	{
+//		//Debug Message Labels
+//		std::cerr << "Objects (" << messageData -> objectCount << "):\n";
+//
+//		for(std::size_t i{}; i < messageData -> objectCount; ++i)
+//		{
+//			std::cerr																	\
+//			<< "\t[" << i << "][OBJECT: " << messageData -> pObjects[i].objectType		\
+//			<< "] Name: \"" << messageData -> pObjects[i].pObjectName << "\""			\
+//			<< " Location: " << (void*)(messageData -> pObjects[i].objectHandle) << "\n";
+//		}
+//	}
+
+	//Check if Command Buffers Exist
+	if(messageData -> cmdBufLabelCount > 0 && messageData -> pCmdBufLabels)
+	{
+		//Debug Command Buffer Labels
+		std::cerr << "Command Buffer Labels (" << messageData -> cmdBufLabelCount << "):\n";
+
+		for(std::size_t i{}; i < messageData -> cmdBufLabelCount; ++i)
+		{
+			std::cerr << "\t[" << i << "][COMMAND BUFFER] \"" << messageData -> pCmdBufLabels[i].pLabelName << "\"\n";
+		}
+	}
+
+	//Print Extra Newline Character
+	std::cerr << '\n';
 }
 
 TestEngine::TestEngine::TestEngine(const bool debug, const bool verbose) : isDebug(debug), isVerbose(verbose)
