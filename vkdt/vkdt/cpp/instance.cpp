79f08d01ec3b7454870ec20811d2a5f0649ba2ef
Hudson Regis <hudsonregis@Hudsons-MacBook-Air.local>
2023-11-15 21:29:10 -0800

    Removed Debug Messenger Related Functions

--- vkdt/vkdt/cpp/instance.cpp
+++ vkdt/vkdt/cpp/instance.cpp
@@ -2,37 +2,8 @@
 #include <vkdt/instance.h>
 
 //Include Headers
-
-/**
- * @brief Vulkan Needed Implementation of VKDT Callback
- *
- * @param messageSeverity Severity of Vulkan Message to Debug
- * @param messageType Type of Vulkan Message to Debug
- * @param pCallbackData Information on Vulkan Message to Debug
- * @param pUserData Optional Data
- *
- * @return VK_FALSE, Mandatory Return Statement
- */
-static VKAPI_ATTR VkBool32 VKAPI_CALL vkdtVKDebugCallback
-(
-	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-	VkDebugUtilsMessageTypeFlagsEXT messageType,
-	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-	void* pUserData
-)
-{
-	//Check if User Data Contains Anything
-	if(pUserData)
-	{
-		throw std::runtime_error("VKDT Vulkan Debug Callback Function Contains Invalid Information!\n");
-	}
-
-	//Call VKDT Debug Function
-	vkdt::debug::vkdtCallbackFunc(messageSeverity, messageType, pCallbackData);
-
-	//Mandatory Return Statement
-	return VK_FALSE;
-}
+#include <vkdt/debug.h>
+#include <vkdt/_pVKObjects.h>
 
 vkdt::instance::instance::instance
 (
@@ -336,139 +307,6 @@ void vkdt::instance::instance::createVKDTInstance(vkdt::debug::vkdtCallback call
 	}
 }
 
-void vkdt::instance::instance::createVKDTDebugMessenger(void)
-{
-	//Stop if not Debugging
-	if(this -> debug == false && this -> verbose == false)
-	{
-		return;
-	}
-
-	//Create & Fill Vulkan Debug Utils Creation Information Struct
-	VkDebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo = {};
-	this -> fillDebugUtilsCreateInfo(debugMessengerCreateInfo);
-
-	//Locate & Load Function to Create Vulkan Debug Utils Messenger
-	const PFN_vkCreateDebugUtilsMessengerEXT createVKDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>
-	(
-		vkGetInstanceProcAddr
-		(
-			this -> vkdtVKInstance,
-			"vkCreateDebugUtilsMessengerEXT"
-		)
-	);
-
-	//Check if Function Created Successfully
-	if(!createVKDebugUtilsMessengerEXT)
-	{
-		throw std::runtime_error("Failed to Locate & Load VKDT Vulkan Debug Utils Messenger Creation Function!\n");
-	}
-	else
-	{
-		//Debug Function Location Success
-		if(this -> verbose)
-		{
-			std::cout << "Successfully Located & Loaded VKDT Vulkan Create Debug Utils Messenger Function!\n";
-		}
-	}
-
-	//Create Debug Utils Messenger
-	const VkResult vkdtVKCreateDebugUtilsMessengerResult = createVKDebugUtilsMessengerEXT
-	(
-		this -> vkdtVKInstance,
-		&debugMessengerCreateInfo,
-		this -> pAllocator,
-		&this -> vkdtVKDebugMessenger
-	);
-
-	if(vkdtVKCreateDebugUtilsMessengerResult == VK_SUCCESS)
-	{
-		//Debug Creation Success
-		if(this -> verbose)
-		{
-			std::cout << "Successfully Created VKDT Debug Utils Messenger, Simulating Function...\n";
-
-			//Vulkan Callback Data Information
-			const VkDebugUtilsMessengerCallbackDataEXT callbackData =
-			{
-				//Struct Type
-				.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CALLBACK_DATA_EXT,
-
-				//Message Information
-				.pMessageIdName = "VKDT Callback",
-				.pMessage = "VKDT Simulation of Implemented Callback Function, Caused by Verbose Option in Settings!",
-				.messageIdNumber = -1,
-			};
-
-			//Call Function
-			vkdtVKDebugCallback
-			(
-				VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT,
-				VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT,
-				&callbackData,
-				VK_NULL_HANDLE
-			);
-		}
-
-		else if(this -> debug)
-		{
-			std::cout << "Successfully Created VKDT Debug Utils Messenger!\n";
-		}
-	}
-	else
-	{
-		throw std::runtime_error
-		(
-			"Failed to Create VKDT Vulkan Debug Utils Messenger! Error: " +
-			std::to_string(vkdtVKCreateDebugUtilsMessengerResult) + "!\n"
-		);
-	}
-}
-
-void vkdt::instance::instance::destroyVKDTDebugMessenger(void)
-{
-	//Destroy Debug Utils Messenger
-	if(this -> debug || this -> verbose)
-	{
-		//Locate & Load Function to Destroy Vulkan Debug Utils
-		const PFN_vkDestroyDebugUtilsMessengerEXT destroyVKDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>
-		(
-			vkGetInstanceProcAddr
-			(
-				this -> vkdtVKInstance,
-				"vkDestroyDebugUtilsMessengerEXT"
-			)
-		);
-
-		//Check if Function is Created Successfully
-		if(!destroyVKDebugUtilsMessengerEXT)
-		{
-			throw std::runtime_error("Failed to Locate & Load VKDT Vulkan Debug Utils Messenger Creation Function!\n");
-		}
-		else
-		{
-			//Debug Function Location Success
-			if(this -> verbose)
-			{
-				std::cout << "Successfully Located & Loaded VKDT Vulkan Destroy Debug Utils Messenger Function!\n";
-			}
-		}
-
-		//Call Function to Vulkan Destroy Debug Utils Messenger
-		destroyVKDebugUtilsMessengerEXT(this -> vkdtVKInstance, this -> vkdtVKDebugMessenger, this -> pAllocator);
-	}
-}
-
-const VkInstance& vkdt::instance::instance::refVKInstance(void) const noexcept
-{
-	return this -> vkdtVKInstance;
-}
-
-const VkDebugUtilsMessengerEXT& vkdt::instance::instance::refVKDebugMessenger(void) const noexcept
-{
-	return this -> vkdtVKDebugMessenger;
-}
-
 bool vkdt::instance::instance::requestedExtensionsSupported(void) const
 {
 	//Check Vulkan Extensions
@@ -641,32 +479,6 @@ bool vkdt::instance::instance::requestedLayersSupported(void) const
 
 	//All Tests Have Passed
 	return true;
-}
-
-inline void vkdt::instance::instance::fillDebugUtilsCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& debugMessengerCreateInfo)const noexcept
-{
-	debugMessengerCreateInfo =
-	{
-		//Struct Type
-		.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-
-		//Message Severity to Debug
-		.messageSeverity =																			\
-		VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |											\
-		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |											\
-		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
-
-
-		//Message Types to Debug
-		.messageType =																				\
-		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |											\
-		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT  |											\
-		VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT,
-
-		//Debug Callback Function
-		.pfnUserCallback = vkdtVKDebugCallback,
-	};
-}
 
 vkdt::instance::instance::~instance() noexcept(false)
 {
