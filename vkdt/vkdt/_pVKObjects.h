f0d79412928ed616c2032b859b88ca570684382c
Hudson Regis <hudsonregis@Hudsons-MacBook-Air.local>
2023-11-15 21:24:19 -0800

    Added Global File to access pointers to Objects

---
+++ vkdt/vkdt/_pVKObjects.h
@@ -0,0 +1,17 @@
+//Header Guard
+#pragma once
+
+//Include Headers
+#include <vkdt/_vkdtConfig.h>
+
+namespace vkdt::_pVKObjects
+{
+	/* Pointers to GLFW Objects */
+	extern GLFWwindow** pGLFWWindow;  //Pointer to GLFW Window
+
+	/* Pointers to Vulkan Objects */
+	extern VkDebugUtilsMessengerEXT* pVKDebugMessenger;  //Pointer to Vulkan Debug Messenger
+	extern VkInstance* pVKInstance;  //Pointer to Vulkan Instance
+	extern VkDevice* pVKLogicalDevice;  //Pointer to Vulkan Logical Device
+	extern VkPhysicalDevice* pVKPhysicalDevice;  //Pointer to Vulkan Physical Device
+}
